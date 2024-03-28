405fa5a5a08e0fbb39565dd04093a55533fce7d9
itsk2 <codonkellyn3@gmail.com>
2024-03-28 18:33:40 +0800

    AppointmentListUI

--- reactexpo/App/Screens/AppointmentsCard.js
+++ reactexpo/App/Screens/AppointmentsCard.js
@@ -1,6 +1,11 @@
 import { View, Text, Image } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesome6 } from '@expo/vector-icons';
+import Colors from '../../assets/Shared/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import moment from 'moment';
 
 const AppointmentsCard = ({ appointment }) => {
 
@@ -17,7 +22,7 @@ const AppointmentsCard = ({ appointment }) => {
 
     const fetchService = async () => {
         try {
-            const response = await axios.get('http://192.168.100.47:8000/get-service-to-appoint', {
+            const response = await axios.get('http://192.168.55.100:8000/get-service-to-appoint', {
                 // const response = await axios.get('http://192.168.137.190:8000/get-service-to-appoint', {
                 params: {
                     serviceId: appointment.service
@@ -31,7 +36,7 @@ const AppointmentsCard = ({ appointment }) => {
 
     const fetchDoctor = async () => {
         try {
-            const response = await axios.get('http://192.168.100.47:8000/get-doctor-to-appoint', {
+            const response = await axios.get('http://192.168.55.100:8000/get-doctor-to-appoint', {
                 // const response = await axios.get('http://192.168.137.190:8000/get-doctor-to-appoint', {
                 params: {
                     doctorId: appointment.doctor
@@ -43,20 +48,42 @@ const AppointmentsCard = ({ appointment }) => {
         }
     };
     return (
-        <View style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginBottom: 30, borderRadius: 12 }}>
-            <Text style={{ fontWeight: 'bold' }}>Date: {appointment.date} - {appointment.time}</Text>
-            <View style={{ flexDirection: 'row' }}>
+        <View style={{ borderWidth: 1,  padding: 10, marginBottom: 10, borderRadius: 12 ,borderColor:Colors.LIGHT_GRAY,backgroundColor:Colors.white, marginTop: 15}}>
+            <Text style={{ fontWeight: 'bold', fontSize:18, fontFamily: 'sans-serif' }}>{moment(appointment.date).format('MMM Do, YYYY')} - {appointment.time}</Text>
+            <View style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 10, alignItems: 'center'
+            }}>
                 {doctor && doctor.image && doctor.image.length > 0 && (
                     <Image
                         source={{ uri: doctor.image[0].url }}
-                        style={{ width: 125, height: 150, borderRadius: 100 }}
+                        style={{ width: 90, height: 100, borderRadius: 10 }}
                     />)}
+
+                <View>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', fontFamily: 'sans-serif' }}>{service.name}</Text>
+                    <View style={{ display: 'flex', flexDirection: 'row', gap: 5, alignItems: 'center'}}>
+                        <FontAwesome6 name="user-doctor" size={17} color="blue" />
+                        <Text style={{ fontSize: 15, fontFamily: 'sans-serif' }}>{doctor.name}</Text>
+                    
+                    </View>
+
+                    <View style={{ display: 'flex', flexDirection: 'row', gap: 3, alignItems: 'center' , marginTop:5}}>
+                        <FontAwesome5 name="file-medical-alt" size={17} color="blue" />
+                        <Text style={{ fontSize: 15, fontFamily: 'sans-serif'}}>Id: #{appointment.service}</Text>
+                    </View>
+                    <View style={{ display: 'flex', flexDirection: 'row', gap: 5, alignItems: 'center',marginTop:5 }}>
+                       <Ionicons name="document-text" size={17} color="blue" />
+                        <Text style={{ fontWeight: 'bold', fontFamily: 'sans-serif'}}>Status: {appointment.status}</Text>
+                    </View>
+                </View>
             </View>
-            {/* <Text style={{ fontWeight: 'bold' }}>Doctor: {appointment.doctor}</Text> */}
-            <Text style={{ fontWeight: 'bold' }}>Service: {appointment.service}</Text>
-            <Text style={{ fontWeight: 'bold' }}>Status: {appointment.status}</Text>
-            <Text style={{ fontWeight: 'bold' }}>Note: {appointment.note}</Text>
+
+
         </View>
+
+
     );
 };
 
