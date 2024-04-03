4215e11ccb53129c7d4ca4a87e878c61a1ddb66d
itsk2 <codonkellyn3@gmail.com>
2024-04-03 09:21:19 +0800

    Modified

--- reactexpo/assets/common/baseURL.js
+++ reactexpo/assets/common/baseURL.js
@@ -19,8 +19,12 @@ let baseURL = '';
 // ? baseURL = 'http://192.168.55.100:8000/api/v1'
 // : baseURL = 'http://192.168.55.100:8000/api/v1'
 
-? baseURL = 'http://192.168.100.47:8000/api/v1'
-: baseURL = 'http://192.168.100.47:8000/api/v1'
+// ? baseURL = 'http://192.168.100.47:8000/api/v1'
+// : baseURL = 'http://192.168.100.47:8000/api/v1'
+
+? baseURL = 'http://172.34.96.111:8000/api/v1'
+: baseURL = 'http://172.34.96.111:8000/api/v1'
+
 }
 
 export default baseURL;
