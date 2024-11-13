2b819d493802c781348fef4b4ab95e7e68d3cab0
Lawliet <141336872+JunnieYAHH@users.noreply.github.com>
2024-11-13 23:44:25 +0800

    Update app.js

--- Services/app.js
+++ Services/app.js
@@ -23,17 +23,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://gerelitopuyos:gerelitopuyos@atlascluster.7cyczkf.mongodb.net/ClinicAppointment?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-});
-
 // Routes
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/services', serviceRoutes);
